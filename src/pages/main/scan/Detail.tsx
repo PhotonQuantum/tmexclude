5ca9c31ab1d63eda3c6ea4b71ba723b702d3eb6c
LightQuantum <self@lightquantum.me>
2022-11-30 23:09:00 +0800

    fix(detail): being slow and overflow for large amount of items

--- src/pages/main/scan/Detail.tsx
+++ src/pages/main/scan/Detail.tsx
@@ -1,7 +1,7 @@
 'use client';
-import {Button, Stack, Text} from "@mantine/core";
-import {IconChevronLeft} from "@tabler/icons";
-import React from "react";
+import {Button, Group, Stack, Text, Tooltip} from "@mantine/core";
+import {IconAlertTriangle, IconChevronLeft} from "@tabler/icons";
+import React, {useState} from "react";
 import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 import {
   actionBatchState,
@@ -19,6 +19,16 @@ export const Detail = React.forwardRef(() => {
   const [removeSelection, setRemoveSelection] = useRecoilState(selectedRemoveActionBatchState);
   const setScanPage = useSetRecoilState(scanPageState);
 
+  const [addTrunc, setAddTrunc] = useState<number | null>(null);
+  const [removeTrunc, setRemoveTrunc] = useState<number | null>(null);
+
+  const truncatedNote = (count: number) => (<Tooltip label={`Showing 100/${count} rows, please refine your search.`}>
+    <Group spacing={"xs"}>
+      <Text size={"xs"} color={"orange"}>Truncated</Text>
+      <IconAlertTriangle size={16} strokeWidth={1} color={"orange"}/>
+    </Group>
+  </Tooltip>);
+
   return (
     <motion.div key={"detail"} style={{height: "100%"}} {...slideFadeAnimation}>
       <Stack py={"xl"} sx={{
@@ -30,15 +40,23 @@ export const Detail = React.forwardRef(() => {
           Back
         </Button>
         <Stack spacing={"xs"} sx={{height: "100%"}}>
-          <Text size={"xs"} color={"dimmed"}>Files to be excluded</Text>
+          <Group position={"apart"}>
+            <Text size={"xs"} color={"dimmed"}>Files to be excluded</Text>
+            {addTrunc !== null && truncatedNote(addTrunc)}
+          </Group>
           <SelectionTable
             sx={{flexGrow: 1, flexBasis: 0}}
-            data={addData} selection={addSelection} onChange={setAddSelection}
+            data={addData} selection={addSelection}
+            limit={100} onChange={setAddSelection} onTruncated={setAddTrunc}
           />
-          <Text size={"xs"} color={"dimmed"}>Files to be included</Text>
+          <Group position={"apart"}>
+            <Text size={"xs"} color={"dimmed"}>Files to be included</Text>
+            {removeTrunc !== null && truncatedNote(removeTrunc)}
+          </Group>
           <SelectionTable
             sx={{flexGrow: 1, flexBasis: 0}}
-            data={removeData} selection={removeSelection} onChange={setRemoveSelection}
+            data={removeData} selection={removeSelection}
+            limit={100} onChange={setRemoveSelection} onTruncated={setRemoveTrunc}
           />
         </Stack>
       </Stack>
