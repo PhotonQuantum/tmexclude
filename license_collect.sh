4cebe8e7674ae2ebdc8b3857c69c4ba147fdc8c2
LightQuantum <self@lightquantum.me>
2022-12-07 01:26:11 +0800

    chore: update acknowledgements

--- license_collect.sh
+++ license_collect.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p jq cargo-license nodejs nodePackages.pnpm
 
 # This script collects the license information for all the dependencies
 
-CARGO_LICENSES=$(cargo license --manifest-path src-tauri/Cargo.toml --direct-deps-only --json | jq 'map(select(.license != null and .license != "") | {name, repository, license, version})|unique_by(.name)')
+CARGO_LICENSES=$(cargo-license --manifest-path src-tauri/Cargo.toml --direct-deps-only --json | jq 'map(select(.license != null and .license != "") | {name, repository, license, version})|unique_by(.name)')
 NPM_LICENSES=$(pnpx license-checker --direct --json | jq 'to_entries|map(select(.value.licenses != null and .value.licenses != "" and (.key | contains("tmexclude") | not) ) | {name:.key, repository:.value.repository, license:.value.licenses})')
 
 cat <<EOF >src/licenses.ts
@@ -12,6 +13,6 @@ export type License = {
   repository: string | null;
   license: string;
 };
-export const cargoLicenses: Array<License> = $CARGO_LICENSES
-export const npmLicenses: Array<License> = $NPM_LICENSES
+export const cargoLicenses: Array<License> = $CARGO_LICENSES;
+export const npmLicenses: Array<License> = $NPM_LICENSES;
 EOF
